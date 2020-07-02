a989a36717c2ed54833c8f6d05c24a8a30731dff
Issam sakr <sakrissam@gmail.com>
2020-07-02 18:08:33 +0200

    Initial commit

---
+++ Zoo/src/main/java/de/stl/saar/internetentw1/myzoo/controller/ZooController.java
@@ -0,0 +1,24 @@
+package de.stl.saar.internetentw1.myzoo.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.stl.saar.internetentw1.myzoo.database.Database;
+import de.stl.saar.internetentw1.myzoo.model.Animal;
+import de.stl.saar.internetentw1.myzoo.model.Compound;
+
+/**
+ * Der Controller steuert die Navigation innerhalb der Anwendung. Die Methoden
+ * geben an, wohin die Navigation beim Aufruf einer bestimmten URL erfolgen soll,
+ * nehmen Request-Parameter entgegen, die von Formularen gesendet wurden, und 
+ * schreiben neue Parameter in die Requests.
+ * @author christopher
+ *
+ */
+@Controller
+public class ZooController {
+	
+}
